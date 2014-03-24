24e7bdc425c23d537b9e7717b2c760aa09a5cc7c
Wesley Schwengle <wesley@schwengle.net>
2014-03-24 11:53:00 +0100

    Initial commit, files as created by Module-Starter.

---
+++ lib/Webservice/BitBucket.pm
@@ -0,0 +1,107 @@
+package Webservice::BitBucket;
+
+use 5.006;
+use strict;
+use warnings FATAL => 'all';
+
+=head1 NAME
+
+Webservice::BitBucket - The great new Webservice::BitBucket!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Webservice::BitBucket;
+
+    my $foo = Webservice::BitBucket->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Wesley Schwengle, C<< <wesley at mintlab.nl> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-webservice-bitbucket at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Webservice-BitBucket>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Webservice::BitBucket
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Webservice-BitBucket>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Webservice-BitBucket>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Webservice-BitBucket>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Webservice-BitBucket/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2014 Wesley Schwengle.
+
+This program is released under the following license: EUPL
+
+
+=cut
+
+1; # End of Webservice::BitBucket
